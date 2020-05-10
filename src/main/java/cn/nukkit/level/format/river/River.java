99e885a6bf05a1588cb02016fbbaf1f1cd0a1dd4
Hasan Demirtaş <utsukushihito@outlook.com>
2020-05-10 03:42:55 +0300

    Update River.java

--- src/main/java/cn/nukkit/level/format/river/River.java
+++ src/main/java/cn/nukkit/level/format/river/River.java
@@ -5,40 +5,41 @@ import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.format.generic.BaseFullChunk;
 import cn.nukkit.level.format.generic.BaseLevelProvider;
 import cn.nukkit.scheduler.AsyncTask;
-
 import java.io.IOException;
 
 public class River extends BaseLevelProvider {
 
-    public River(Level level, String path, boolean f) throws IOException {
+    public River(final Level level, final String path, final boolean f) throws IOException {
         super(level, path, f);
     }
 
-    public String getProviderName() {
+    public static String getProviderName() {
         return "river";
     }
+
     @Override
-    public BaseFullChunk loadChunk(long index, int chunkX, int chunkZ, boolean create) {
+    public BaseFullChunk loadChunk(final long index, final int chunkX, final int chunkZ, final boolean create) {
         return null;
     }
 
     @Override
-    public AsyncTask requestChunkTask(int X, int Z) {
+    public AsyncTask requestChunkTask(final int X, final int Z) {
         return null;
     }
 
     @Override
-    public BaseFullChunk getEmptyChunk(int x, int z) {
+    public BaseFullChunk getEmptyChunk(final int x, final int z) {
         return null;
     }
 
     @Override
-    public void saveChunk(int X, int Z) {
+    public void saveChunk(final int X, final int Z) {
 
     }
 
     @Override
-    public void saveChunk(int X, int Z, FullChunk chunk) {
+    public void saveChunk(final int X, final int Z, final FullChunk chunk) {
 
     }
+
 }
